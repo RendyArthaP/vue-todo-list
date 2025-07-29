79f85f15c13f109d51debebe827d19d979688a28
Rendy Artha <rendyartha312@gmail.com>
2025-07-29 17:06:50 +0700

    feat: create todo list and mapping list when user input

---
+++ src/components/ButtonTodo.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="btn" @click="onClick" :disabled="disabled">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface ButtonProps {
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const props = defineProps<ButtonProps>();
+const emit = defineEmits<{
+  (event: "click"): void;
+}>();
+
+const onClick = () => {
+  if (props.disabled) return;
+  emit("click");
+};
+</script>
+
+<style lang="css">
+.btn {
+  padding: 10px 20px;
+  border: 1px solid #007bff;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/InputText.vue
@@ -0,0 +1,36 @@
+<template>
+  <input
+    type="text"
+    :value="modelValue"
+    @input="onInput"
+    placeholder="Input your To Do..."
+    class="input"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  modelValue: string;
+}
+
+const { modelValue } = defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+const onInput = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+};
+</script>
+
+<style scoped lang="css">
+.input {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 100%;
+  font-size: 14px;
+}
+</style>
